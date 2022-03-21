c6741f93a1cf543ca72ea14d00496e9ba6a3bfa1
Jade <jade.cmail@gmail.com>
2022-03-21 22:27:00 +0100

    added vinylComponent delete, modify methods + get media method in ngOnInit

--- src/app/cd/cds.component.ts
+++ src/app/cd/cds.component.ts
@@ -5,24 +5,16 @@ import { MediaService } from '../shared/services/media.service';
 @Component({
   selector: 'app-cds',
   templateUrl: './cds.component.html',
-  styleUrls: ['./cds.component.css']
+  styleUrls: ['./cds.component.css'],
 })
 export class CdsComponent implements OnInit {
   cds: Media[];
 
-  constructor(private mediaService : MediaService) { 
+  constructor(private mediaService: MediaService) {
     this.cds = [];
   }
 
   ngOnInit(): void {
-    this.mediaService.getMedias().subscribe((medias : Media[]) => {
-      medias.forEach((media :Media) => {
-        if(media.type.id == 2){
-          this.cds.push(media)
-        }
-      })
-
-    })
+    
   }
-
 }
